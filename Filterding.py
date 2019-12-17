68292999dfc1669d8a603045ff934f5351db63f5
BramUt <bram.utberg@gmail.com>
2019-12-17 20:53:07 +0100

    Filter functioneert

--- Filterding.py
+++ Filterding.py
@@ -1,15 +1,19 @@
 from tkinter import *
 from tkinter import filedialog
 
+root = Tk()
+root.withdraw()
+
 
 class MainGUI:
 
     def __init__(self):
+        file = filedialog.askopenfilename(title="Select a file.",
+                                          initialdir=".")
+
         window = Tk()
         window.title("Filterding")
         window.geometry("700x400")
-
-        file = filedialog.askopenfilename(title="Select a file.")
 
         exit_button = Button(window, text="Exit",
                              command=lambda: window.quit())
@@ -18,9 +22,75 @@ class MainGUI:
         window.mainloop()
 
 
-def main():
+def file_opener():
+    """Opens a window asking the user to select a file. Returns this path."""
+    return filedialog.askopenfilename(title="Select a file.",
+                                      initialdir=".")
 
-    gui = MainGUI()
+
+def file_reader():
+    candidates = []
+
+    with open(file_opener()) as file:
+        for counter, line in enumerate(file):
+            if not counter:
+                header_line = line.rstrip().split("\t")
+
+                print(header_line)
+                reads_i = header_line.index("reads")
+                phylop_i = header_line.index("phyloP")
+                var_reads_i = header_line.index("variation reads")
+                perc_var_i = header_line.index("% variation")
+                snp_i = header_line.index("SNP id")
+                synonymous_i = header_line.index("Synonymous")
+                gen_comp_i = header_line.index("Gene component")
+                print(reads_i, phylop_i, var_reads_i, perc_var_i, snp_i,
+                      synonymous_i, gen_comp_i)
+            else:
+                try:
+                    line = line.rstrip().split("\t")
+                    if (
+                            float(line[phylop_i]) >= 2.5 and
+                            int(line[reads_i]) >= 5 and
+                            line[snp_i] == "" and
+                            int(line[var_reads_i]) >= 5 and
+                            int(line[perc_var_i]) >= 20 and
+                            line[synonymous_i] == "FALSE" and
+                            line[gen_comp_i] in ("EXON_REGION", "SA_SITE")
+                    ):
+                        # print(line)
+                        candidates.append(line)
+                except IndexError:
+                    print(line)
+            if counter == 1:
+                print(line)
+
+    return candidates, header_line
+
+
+def file_writer(candidates, header_line):
+
+    while True:
+        try:
+            with open("Filterding results.tsv", "w") as file:
+                file.write("\t".join(header_line) + "\n")
+                for c in candidates:
+                    file.write("\t".join(c) + "\n")
+                break
+        except PermissionError:
+            input("Permission to denied 'Filterding results.tsv'")
+
+
+def filter_func():
+    pass
+
+
+def main():
+    # gui = MainGUI()
+
+    candidates, header_line = file_reader()
+
+    file_writer(candidates, header_line)
 
 
 main()
